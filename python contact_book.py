4283b1f1b2c00e9def59e36c654845cf22f83fd1
sakshi-694 <sakshimehra694@gmail.com>
2026-01-22 13:18:20 +0530

    Create python contact_book.py

---
+++ python contact_book.py
@@ -0,0 +1,9 @@
+
+## 📌 Learning Outcomes
+- Understanding Python syntax
+- Working with conditionals and loops
+- File handling in Python
+- Building a mini CLI application
+
+## 👩‍💻 Author
+**Sakshi Mehra**
